48c832f27bbe829c6a6a3b4fc9c49fac5b851c08
Charles Nascimento <charles.si.ufs@gmail.com>
2017-08-01 20:26:30 -0300

    Initial commit.

---
+++ app/src/main/cpp/Application.mk
@@ -0,0 +1,7 @@
+#APP_CPPFLAGS += -fexceptions
+
+APP_BUILD_SCRIPT = $(APP_PROJECT_PATH)/Android.mk
+
+compile-s-source = $(eval $(call ev-compile-c-source,$1,$(1:%.s=%.o)))
+
+JNI_H_INCLUDE = $(APP_PROJECT_PATH)/common/libnativehelper/include/
